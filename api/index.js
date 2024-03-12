78e49d07fc55c4e304a6e82dd6f0508c7dbd8d09
Khushal Mali <khushalmali8448@gmail.com>
2024-03-12 19:20:43 +0530

    connected to database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,18 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 const app = express();
+dotenv.config();
 
-app.listen(3000, () => {
-  console.log(`Server listening on --PORT : 3000`);
-});
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then((res) =>
+    app.listen(3000, () => {
+      console.log(`--HOST: ${res.connection.host}`, `--PORT: ${res.connection.port}`);
+      console.log(`Server listening on --PORT : 3000`);
+    })
+  )
+  .catch((err) => console.error(err));
+
+// ;
