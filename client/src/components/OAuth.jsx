7eefb2f43e44d89f69811ed8f9b4d4002720dda2
Khushal Mali <khushalmali8448@gmail.com>
2024-03-14 22:09:45 +0530

    OAuth completed on client side

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -1,7 +1,28 @@
+import { GoogleAuthProvider, signInWithPopup, getAuth } from "firebase/auth";
+import { app } from "../firebase";
+import axios from "axios";
+import { useDispatch } from "react-redux";
+import { signInSuccess } from "../redux/user/userSlice";
+
 function OAuth() {
+  const dispatch = useDispatch();
+
   async function handleGoogleClick() {
     try {
-      //
+      const provider = new GoogleAuthProvider();
+      const auth = getAuth(app);
+      const result = await signInWithPopup(auth, provider);
+      const res = await axios({
+        method: "post",
+        url: "/api/v1/auth/google",
+        data: {
+          name: result.user.displayName,
+          email: result.user.email,
+          photo: result.user.photoURL,
+        },
+      });
+      dispatch(signInSuccess(res.data.data));
+      console.log(result);
     } catch (error) {
       console.error("Could not login with google", error);
     }
