689255ca7bd7ab25ada04f71f34b54867b968065
Khushal Mali <khushalmali8448@gmail.com>
2024-03-14 22:09:09 +0530

    changed name of firebase file

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -5,7 +5,7 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCrrY3a4l3oabUM7XNL_nJP_8KpENTDqFE",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: "mernauth-d88e5.firebaseapp.com",
   projectId: "mernauth-d88e5",
   storageBucket: "mernauth-d88e5.appspot.com",
@@ -14,4 +14,4 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
