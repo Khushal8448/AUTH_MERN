bca64dd7ed94377296e4c59b27d0291c678effd7
Khushal Mali <khushalmali8448@gmail.com>
2024-03-14 05:30:21 +0530

    created signin backend functionality

--- server/api/controllers/auth.controller.js
+++ server/api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signUp = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -23,5 +24,41 @@ export const signUp = async (req, res, next) => {
     res.status(409).json({
       message: `User already exists`,
     });
+  }
+};
+export const signIn = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  try {
+    const validUser = await User.findOne({ email });
+
+    // User not found
+    if (!validUser) return next(errorHandler(404, "User not found!"));
+
+    const isPasswordCorrect = bcryptjs.compareSync(password, validUser.password);
+
+    // Wrong password
+    if (!isPasswordCorrect) return next(errorHandler(400, "Invalid Inputs"));
+
+    // Logged In User Response
+    const loggedInUser = await User.findById(validUser._id).select("-password");
+
+    console.log(loggedInUser);
+
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+
+    const expiryDate = new Date(Date.now() + 3600000); // 1 hour
+
+    return res
+      .cookie("access_token", token, { httpOnly: true, secure: true, expires: expiryDate })
+      .status(201)
+      .json({
+        data: loggedInUser,
+        message: "User logged in successfully",
+      });
+  } catch (error) {
+    // Database error
+    console.error("Error retrieving user:", error);
+    return next(errorHandler(500, "Internal Server Error!"));
   }
 };
