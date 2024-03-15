0b0da441629940e1ed5af99d457292629e533964
Khushal Mali <khushalmali8448@gmail.com>
2024-03-15 11:30:12 +0530

    updated header component and make profile private

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,21 +1,35 @@
 import { NavLink } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 function Header() {
+  const { currentUser } = useSelector((state) => state.user);
   return (
     <div className="bg-slate-400">
-      <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+      <div className="mx-auto flex max-w-6xl items-center justify-between p-3">
         <NavLink to="/">
-          <h1 className="font-extrabold text-3xl text-slate-800">Auth App</h1>
+          <h1 className="text-3xl font-extrabold text-slate-800">Auth App</h1>
         </NavLink>
-        <ul className="flex items-center font-semibold gap-4 text-slate-900">
+        <ul className="flex items-center gap-4 font-semibold text-slate-900">
           <NavLink to="/">
             <li>Home</li>
           </NavLink>
           <NavLink to="/about">
             <li>About</li>
           </NavLink>
-          <NavLink to="sign-in">
-            <li>Sign In</li>
+          <NavLink to={currentUser ? "/profile" : "sign-in"}>
+            <li>
+              {currentUser ? (
+                <img
+                  src={currentUser.profilePicture}
+                  width={28}
+                  height={28}
+                  className="rounded-full object-cover"
+                  alt="Profile Picture"
+                />
+              ) : (
+                "Sign In"
+              )}{" "}
+            </li>
           </NavLink>
         </ul>
       </div>
